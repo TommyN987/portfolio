f01a3a188ad26d8cc775494ff93d29cc2f71bda5
TommyN987 <tomas.nagy.tn@gmail.com>
2022-05-06 10:02:02 +0200

    animate intro box

--- src/components/Intro.js
+++ src/components/Intro.js
@@ -1,7 +1,8 @@
 import styled from 'styled-components';
+import { motion } from 'framer-motion';
 import Me from '../assets/profile-img.png'
 
-const Box = styled.div`
+const Box = styled(motion.div)`
   position: absolute;
   left: 50%;
   top: 50%;
@@ -43,10 +44,13 @@ const Text = styled.div`
   font-size: calc(1em + 1.5vw);
   color: ${props => props.theme.body};
   padding: 2rem;
-  cursor: pointer;
   display: flex;
   flex-direction: column;
   justify-content: space-evenly;
+
+  h1,h3 {
+    font-family: 'Philosopher';
+  }
 
   &>*:last-child {
     color: ${props => `rgba(${props.theme.bodyRgba}, .6)`};
@@ -58,7 +62,11 @@ const Text = styled.div`
 const Intro = () => {
   return (
     <div>
-      <Box>
+      <Box
+      initial={{ height: 0 }}
+      animate={{ height: '50vh' }}
+      transition={{ type: 'spring', duration: 2, delay: 1}}
+      >
         <SubBox>
           <Text>
             <h1>Hi,</h1>
@@ -67,9 +75,13 @@ const Intro = () => {
           </Text>
         </SubBox>
         <SubBox>
-          <div>
+          <motion.div
+          initial={{ opacity: 0 }}
+          animate={{ opacity: 1 }}
+          transition={{ duration: 1, delay: 2}}
+          >
             <img className='pic' src={Me} alt="Profile pic" />
-          </div>
+          </motion.div>
         </SubBox>
       
       </Box>
